19de64f7c43107fb7bd519e68b7afd3f607982de
matt mallett <mmallett91@gmail.com>
2014-09-10 21:50:18 -0400

    4 panel layout done, started to add content in

---
+++ js/style.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+
+	$(".full-page-panel").height($(window).height());
+
+});
+
+$(document).resize(function(){
+
+	$(".full-page-panel").height($(window).height());
+
+});
+
