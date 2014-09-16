427f7b8939dc385b54d2e04c764e84c23f46dc99
matt mallett <mmallett91@gmail.com>
2014-09-15 22:45:23 -0400

    auto center the content based on viewport and content height

--- js/style.js
+++ js/style.js
@@ -1,12 +1,71 @@
+var DOWN_ARROW_SIZE = 50;
+
 $(document).ready(function(){
 
 	$(".full-page-panel").height($(window).height());
 
+	// call helper functions to vertically align content
+	// in each panel
+	setSpacerDiv1();
+
+	setSpacerDiv2();
+
+	setSpacerDiv3();
+
+	setSpacerDiv4();
+
 });
 
-$(document).resize(function(){
+// $(document).resize(function(){
 
-	$(".full-page-panel").height($(window).height());
+// 	$(".full-page-panel").height($(window).height());
 
-});
+// });
+
+/*
+ calculating the height of each panel's spacer
+ div so that the elements are roughly centered
+ values are obtained by adding the height of all elements
+ in that panel
+*/
+function setSpacerDiv1(){
+	// ele heights
+	// 218
+	// 32
+	// 149
+	// 140
+	var contentHeight = 539;
+
+	var emptySpace = $(window).height() - contentHeight;
+
+	$("#spacer1").height(emptySpace/2);
+}
+
+function setSpacerDiv2(){
+	var contentHeight = 549;
+
+	var emptySpace = $(window).height() - contentHeight;
+
+	$("#spacer2").height(emptySpace/2);
+}
+
+function setSpacerDiv3(){
+	var contentHeight = 368;
+
+	var emptySpace = $(window).height() - contentHeight;
+
+	$("#spacer3").height(emptySpace/2);
+}
+
+function setSpacerDiv4(){
+	// ele heights
+	// 33
+	// 38	
+	// 368
+	var contentHeight = 439;
+
+	var emptySpace = $(window).height() - contentHeight;
+
+	$("#spacer4").height(emptySpace/2);
+}
 
